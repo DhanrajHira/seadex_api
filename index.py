4ae81be2817a5d27120e7ca2c98744c9f692a765
Dhanrajbir Singh Hira <dhanrajb@ualberta.ca>
2022-01-17 17:39:00 -0700

    Switched from thefuzz to rapidfuzz for better performance.

--- index.py
+++ index.py
@@ -1,47 +1,48 @@
-from thefuzz import process
+from rapidfuzz import process
+from rapidfuzz.fuzz import QRatio
 from asyncio import Event
+
 
 class Index(object):
     def __init__(self, index_list):
         self.__list = index_list
         self.__name_to_series_dict = dict()
         self.__is_ready_event = Event()
+        self.__series_title_list = []
         self.__build_name_to_series_dict()
         self.__is_ready_event.set()
 
     def __build_name_to_series_dict(self):
+        self.__series_title_list.clear()
         for s in self.__list:
             self.__name_to_series_dict[s["title"]] = s
-    
+            self.__series_title_list.append(s["title"])
+
     async def update(self, index_list):
         self.__is_ready_event.clear()
         self.__list = index_list
         self.__build_name_to_series_dict()
         self.__is_ready_event.set()
 
-
     async def search(self, query, limit):
         await self.__is_ready_event.wait()
-        fuzz_results = process.extract(query, self.__series_name_generator(self.__list), limit=limit)
+        fuzz_results = process.extract(
+            query, self.__series_title_list, limit=limit, scorer=QRatio)
         results = []
         for fr in fuzz_results:
             results.append(self.__name_to_series_dict[fr[0]])
         return results
-    
+
     async def get_one(self, query):
         await self.__is_ready_event.wait()
-        fuzz_result = process.extractOne(query, self.__series_name_generator(self.__list))
+        fuzz_result = process.extractOne(
+            query, self.__series_title_list, scorer=QRatio)
         return self.__name_to_series_dict[fuzz_result[0]]
 
     async def clear(self):
         await self.__is_ready_event.wait()
         self.__list.clear()
-        self.__name_to_series_dict.clear() 
+        self.__name_to_series_dict.clear()
 
     def __len__(self):
         return len(self.__list)
-
-    @staticmethod
-    def __series_name_generator(index):
-        for series in index:
-            yield series["title"]
